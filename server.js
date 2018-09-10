db140569b155421f2c0f5fb6c7dd5497eede3a30
Kevin Ndung'u <kevgathuku@gmail.com>
2018-09-10 08:19:01 +0300

    Add server api routes

--- server.js
+++ server.js
@@ -1,9 +1,51 @@
 const Hapi = require("hapi");
+const namespace = require("hapijs-namespace");
+const api = require("./api/museums");
 
 const server = Hapi.server({
   host: "localhost",
-  port: 8000
+  port: 8000,
+  routes: {
+    cors: true
+  }
 });
+
+namespace(server, "/api", [
+  {
+    method: "GET",
+    path: "/",
+    config: {
+      description: "Healthcheck route",
+      handler: function() {
+        return "hello world";
+      }
+    }
+  },
+  {
+    method: "GET",
+    path: "/all",
+    config: {
+      description: "Fetch all items in collection",
+      handler: api.fetchAll
+    }
+  },
+  {
+    method: "GET",
+    path: "/getItem",
+    config: {
+      description: "Fetch individual item in a collection",
+      handler: api.fetchItem
+    }
+  },
+  {
+    method: "GET",
+    path: "/filter",
+    config: {
+      description: "Filter collection items",
+      handler: api.filter
+    }
+  }
+]);
 
 async function start() {
   try {
