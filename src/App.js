8dd6c30c3bc4f0a265daa692e7bd8986bf50cf02
Kevin Ndung'u <kevgathuku@gmail.com>
2018-09-10 08:34:44 +0300

    Add homepage static content

--- src/App.js
+++ src/App.js
@@ -9,9 +9,62 @@ class App extends Component {
           <h1 className="App-title">Welcome to Rijksmuseum</h1>
         </header>
         <main className="container">
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
+          <div className="row mt-40">
+            <div className="col-md-4">
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src="https://placeimg.com/200/200/nature"
+                  alt="Card image cap"
+                />
+                <div className="card-body">
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4"><div className="card">
+                <img
+                  className="card-img-top"
+                  src="https://placeimg.com/200/200/nature"
+                  alt="Card image cap"
+                />
+                <div className="card-body">
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                </div>
+              </div></div>
+            <div className="col-md-4"><div className="card">
+                <img
+                  className="card-img-top"
+                  src="https://placeimg.com/200/200/nature"
+                  alt="Card image cap"
+                />
+                <div className="card-body">
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                </div>
+              </div></div>
+            <div className="col-md-4"><div className="card">
+                <img
+                  className="card-img-top"
+                  src="https://placeimg.com/200/200/nature"
+                  alt="Card image cap"
+                />
+                <div className="card-body">
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                </div>
+              </div></div>
+          </div>
         </main>
       </div>
     );
